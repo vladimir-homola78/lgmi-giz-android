c8aced635d46400e68d7433bb6fdd4cc98289867
Pete <pete@ibrow.com>
2014-11-30 17:43:01 +0100

    weitere infos and impressum views

--- src/main/java/com/ibrow/de/giz/siegelklarheit/InfosActivity.java
+++ src/main/java/com/ibrow/de/giz/siegelklarheit/InfosActivity.java
@@ -3,8 +3,15 @@ package com.ibrow.de.giz.siegelklarheit;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * Activity that shows "weiter infos"
@@ -17,6 +24,37 @@ public class InfosActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_infos);
+
+        WebView html_view=(WebView) findViewById(R.id.info_webview);
+        // WebView.setWebContentsDebuggingEnabled(true); <- needs API 19
+        html_view.getSettings().setJavaScriptEnabled(true);
+
+        StringBuilder buf=new StringBuilder();
+        BufferedReader in=null;
+        try {
+            InputStream is=getAssets().open("html/weitere-infos.html");
+            in= new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            String str;
+
+            while ((str = in.readLine()) != null) {
+                buf.append(str);
+            }
+        }
+        catch (IOException ioe){
+            Log.e("INFOS", ioe.getMessage());
+        }
+        finally {
+            try {
+                in.close();
+            }
+            catch (IOException ioe){
+                Log.e("INFOS", ioe.getMessage());
+            }
+        }
+
+
+        html_view.loadDataWithBaseURL("file:///android_asset/html/", buf.toString(), "text/html", "UTF-8", null);
+        html_view.getSettings().setLoadsImagesAutomatically(true);
     }
 
     /* menu */
